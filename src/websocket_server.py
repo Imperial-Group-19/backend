53e065a93b53a4cfef3f61687f88bb1a971a4beb
Kamil Kurdziel <kk2714@ic.ac.uk>
2022-01-23 23:40:07 +0000

    Fixing ws server.

--- src/websocket_server.py
+++ src/websocket_server.py
@@ -18,33 +18,29 @@ from messaging_protocol import Subscription, Message
 class WebSocketServer(WebSocketServerFactory):
 
     class ServerProtocol(WebSocketServerProtocol):
-        def __init__(self, web_socket_server):
-            self.__websocket_server = web_socket_server
-            self.__logging = self.__websocket_server.logging
-            self.__request: ConnectionRequest = None
-            super().__init__()
 
         def onConnect(self, request: ConnectionRequest):
             self.__request = request
-            self.__logging.info(f"Client connecting: {self.__request.peer}")
-            self.__websocket_server.add_remote_client(self)
+            self.factory.logging.warning(f"Client connecting: {self.__request.peer}")
+            self.factory.add_remote_client(self)
 
         def onOpen(self):
-            self.__logging.info(f"WebsocketConnection open for: {self.__request.peer}")
+            self.factory.logging.warning(f"WebsocketConnection open for: {self.__request.peer}")
 
         def onMessage(self, payload: Union[bytes, str], is_binary: bool):
             if is_binary:
-                self.__logging.debug(f"Binary message received from: {self.__request.peer}: msg: {payload}")
+                self.factory.logging.warning(f"Binary message received from: {self.__request.peer}: msg: {payload}")
             else:
-                self.__logging.debug(f"Binary message received from: {self.__request.peer}: msg: {payload.decode('utf8')}")
+                self.factory.logging.warning(f"Binary message received from: {self.__request.peer}: msg: {payload.decode('utf8')}")
 
             # handle msgs here - read json/binaries + give callbacks
 
         def onClose(self, was_clean: bool, code: int, reason: str):
-            self.__logging.info(f"Client disconnecting: {self.__request.peer}: {was_clean=}, {code=}, {reason=}")
-            self.__websocket_server.remove_remote_client(self.remote_address)
+            self.factory.logging.warning(f"Client disconnecting: {self.__request.peer}: {was_clean=}, {code=}, {reason=}")
+            self.factory.remove_remote_client(self.remote_address)
 
         def send_msg(self, data: bytes):
+            self.factory.logging.warning(f"Sending bytes: {data}")
             self.sendMessage(data, True)
 
         def close_connection(self, code: WebSocketProtocol.CLOSE_STATUS_CODES_ALLOWED, reason: str):
@@ -61,14 +57,17 @@ class WebSocketServer(WebSocketServerFactory):
         self.event_loop: asyncio.AbstractEventLoop = asyncio.get_event_loop()
 
         ws_address = f"ws://127.0.0.1:{self.port}"
-        super().__init__(ws_address)
+        super(WebSocketServer, self).__init__(ws_address)
+        # this is dodgy - but works :eyes: python....
+        self.ServerProtocol.factory = self
+        self.protocol = self.ServerProtocol
 
         # sets of clients and subscribers here
         self.__connected_clients = set()
         self.__subscribed_clients: Dict[Subscription, set] = {}
 
         # add server to asyncio + run until complete
-        self.server = self.event_loop.create_server(protocol_factory=lambda: self.ServerProtocol(self),
+        self.server = self.event_loop.create_server(protocol_factory=self,
                                                     host="", port=self.port)
         self.__async_server_future = self.event_loop.run_until_complete(self.server)
 
@@ -113,10 +112,18 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     log_file = os.path.join(args.log_dir, "middle-tier-service.log")
-    logging.basicConfig(filename=log_file, level=args.log_level)
+    logging.basicConfig(filename=log_file, level=logging.DEBUG)
 
     loop = asyncio.get_event_loop()
     ws_server = WebSocketServer(port=args.port, logging=logging)
+
+    async def dummy_function():
+        while True:
+            subscription = Subscription(type="Kamil")
+            ws_server.send_msg(subscription)
+            await asyncio.sleep(1)
+
+    loop.create_task(dummy_function())
 
     try:
         loop.run_forever()
