272edc50cf97957769d2d9c22f21922fbac27b50
Clarissa Yeo <cy21@vm-shell1.doc.ic.ac.uk>
2022-02-07 23:53:16 +0000

    Updated connect_db.py and dbinfo.py

--- src/connect_db.py
+++ src/connect_db.py
@@ -1,3 +1,4 @@
+from typing import List
 import dataclasses
 import psycopg2
 from dbinfo import User, Transaction
@@ -5,9 +6,8 @@ from dbinfo import User, Transaction
 class postgresDBClient:
     def __init__(self, db_type):
         # Connection establishment
-        # self.conn = conn
         self.db_type = db_type
-        conn = psycopg2.connect(
+        self.conn = psycopg2.connect(
             database=db_type,
             user='postgres',
             password = 'crypto',
@@ -15,11 +15,10 @@ class postgresDBClient:
             port= '5432'
         )
         
-        conn.autocommit = True
+        self.conn.autocommit = True
 
         # Creating a cursor object
-        # self.cursor = cursor
-        cursor= conn.cursor()
+        self.cursor= self.conn.cursor()
 
         # Print when connection is successful
         print("Database has been connected successfully !!");
@@ -48,14 +47,21 @@ class postgresDBClient:
         self.cursor.close()
         self.conn.close()
 
-    def get_users(self):
+
+    # read at initialisation and store data in cache - hash to identify each table 
+    # store dictionary
+    # product dictionary based on store hash 
+    def get_users(self) -> List[User]:
         # Query
         self.cursor.execute("SELECT name, email_add, wallet_id FROM userinfo ORDER BY name")
         rows = self.cursor.fetchall()
         print("The number of users: ", self.cursor.rowcount)
 
+        users = []
         for row in rows:
-            print(row)
+            users.append(User(*row))
+
+        return users
 
 
     def get_transactions(self):
@@ -73,6 +79,7 @@ class postgresDBClient:
 
         # Insert user details 
         insert_user = """ INSERT INTO userinfo (NAME, EMAIL_ADD, WALLET_ID) VALUES (%s,%s,%s)"""
+        # insert_user_str = f"INSERT INTO userinfo (NAME, EMAIL_ADD, WALLET_ID) VALUES ({user})"
 
         self.cursor.execute(insert_user, user_tuple)
 
@@ -101,6 +108,20 @@ class postgresDBClient:
         # Commit changes
         self.conn.commit()
 
+
+#get from store by store_id
+#get from product by store_id then product_id
+
 if __name__ == "__main__":
     db_user = postgresDBClient('users')
     db_transaction = postgresDBClient('transactions')
+
+    users = db_user.get_users()
+    for us in users:
+        print(us)
+        # db_user.add_users(us)
+
+    
+    
+    
+    # db_product_store = postgresDBClient('')
--- src/dbinfo.py
+++ src/dbinfo.py
@@ -1,4 +1,7 @@
 from dataclasses import dataclass
+from turtle import st
+from typing import List
+from unicodedata import name
 
 
 @dataclass(init=True, repr=True, frozen=True)
@@ -7,6 +10,9 @@ class User:
     email_add: str 
     wallet_id: str
 
+    def __str__(self):
+        return f"{self.name}, {self.email_add}, {self.wallet_id}"
+
 @dataclass(init=True, repr=True, frozen=True)
 class Transaction:
     payer_id: str 
@@ -14,3 +20,38 @@ class Transaction:
     coin: str 
     amount: float 
     gas_spend: float
+
+@dataclass(init=True, repr=True, frozen=True)
+class Store:
+    id: str
+    title: str
+    description: str
+
+    def __eq__(self, __o: object) -> bool:
+        if isinstance(__o, Store):
+            if self.id == __o.id:
+                return True
+        return False
+
+    def __hash__(self) -> int:
+        return hash(self.id)
+
+@dataclass(init=True, repr=True, frozen=True)
+class Product:
+    product_id: str
+    store_id: str
+    title: str
+    description: str
+    price: int
+    feature: List[str]
+
+    def __eq__(self, __o: object) -> bool:
+        if isinstance(__o, Store):
+            if self.store_id == __o.store_id and self.product_id == __o.product_id:
+                return True
+        return False
+
+    def __hash__(self) -> int:
+        return hash(self.product_id) ^ hash(self.store_id)
+
+
